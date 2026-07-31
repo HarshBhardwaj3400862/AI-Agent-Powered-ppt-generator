b28e38d8869cb4a1305627b7b8e0b7060c945998
HarshBhardwaj3400862 <hb3400862@gmail.com>
2026-07-31 15:38:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ if all(ALL_API) and user_query:
           unsafe_allow_javascript = True)
   if st.download_button(label = "DOWNLOAD PPT",
                      data = code,
-                     file_name = ppt.html
+                     file_name = ppt.html,
                      mine = 'text/html'):
   
     st.success("PPT DOWNLOADED SUUCCESSFULLY")
