cd288511c5e4c12c50ba366d3e093a3a0056f661
HarshBhardwaj3400862 <hb3400862@gmail.com>
2026-07-31 15:55:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,15 +155,15 @@ if all(ALL_API) and user_query:
         response = agent.invoke({'messages':[{'role':"user",
                                         "content":prompt}]})
   
-  code = response['messages'][-1].content[-1]['text']
-  
-  st.html(code, width = "stretch",
-          unsafe_allow_javascript = True)
-  if st.download_button(label = "DOWNLOAD PPT",
-                     data = code,
-                     file_name = ppt.html,
-                     mine = 'text/html'):
-  
-    st.success("PPT DOWNLOADED SUUCCESSFULLY")
+        code = response['messages'][-1].content[-1]['text']
+        
+        st.html(code, width = "stretch",
+                unsafe_allow_javascript = True)
+        if st.download_button(label = "DOWNLOAD PPT",
+                           data = code,
+                           file_name = ppt.html,
+                           mine = 'text/html'):
+        
+          st.success("PPT DOWNLOADED SUUCCESSFULLY")
   
 
