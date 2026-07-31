66909141474ad22b33dff6062bcf8fad0c4b38a0
HarshBhardwaj3400862 <hb3400862@gmail.com>
2026-07-31 16:15:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ if all(ALL_API) and user_query:
     st.header("generative image give prompt")
     if st.button("CLICK TO GENERATE: ", key = "generate_img_button"):
       with st.spinner("Running agent"):
-        data = f"https://image.pollinations.ai/{user_query}"
+        data =f"https://image.pollinations.ai/{user_query}"
         time.sleep(3)
         st.image(data)
         # st.image("Image.jpeg")
